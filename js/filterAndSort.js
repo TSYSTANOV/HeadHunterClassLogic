71eb3ee1bf75388ea01f57f6dea282ec5fd2d907
TSYSTANOV <tsystanov.vitaly@gmail.com>
2024-04-02 22:50:40 +0300

    continue work

---
+++ js/filterAndSort.js
@@ -0,0 +1,85 @@
+import { VACANCIES_component } from "./renderVacancies.js";
+
+class FilterAndSort {
+  searhPeriodValue = 365;
+  order_ByValue = "date";
+  controller() {
+    const option = document.querySelector(".option");
+    const order = document.querySelector(".option__btn_order");
+    const period = document.querySelector(".option__btn_period");
+    const orderList = option.querySelector(".option__list_order");
+    const periodList = option.querySelector(".option__list_period");
+    order.addEventListener("click", () => {
+      orderList.classList.toggle("option__list_active");
+      periodList.classList.remove("option__list_active");
+    });
+    period.addEventListener("click", () => {
+      periodList.classList.toggle("option__list_active");
+      orderList.classList.remove("option__list_active");
+    });
+    orderList.addEventListener("click", () => {
+      orderList
+        .querySelector(".option__item_active")
+        .classList.remove("option__item_active");
+      order.textContent = event.target.textContent;
+      event.target.classList.add("option__item_active");
+      this.order_ByValue = event.target.dataset.sort;
+      orderList.classList.toggle("option__list_active");
+      this.sortByParams();
+    });
+    periodList.addEventListener("click", () => {
+      periodList
+        .querySelector(".option__item_active")
+        .classList.remove("option__item_active");
+      period.textContent = event.target.textContent;
+      event.target.classList.add("option__item_active");
+      this.searhPeriodValue = event.target.dataset.date;
+      periodList.classList.toggle("option__list_active");
+      this.sortByParams();
+    });
+  }
+  sortByParams(dataArray) {
+    let data = dataArray ? dataArray : VACANCIES_component.DATA_VACANCIES;
+    data = this.sortByOrderValue(data);
+    data = this.sortByPeriodValue(data);
+    if (dataArray) {
+      console.log(1);
+      return data;
+    }
+    VACANCIES_component.removeVacancy();
+    VACANCIES_component.renderVacancy(data);
+  }
+  sortByOrderValue(data) {
+    switch (this.order_ByValue) {
+      case "date":
+        data.sort((a, b) => {
+          return new Date(a.date).getTime() > new Date(b.date).getTime()
+            ? -1
+            : 1;
+        });
+        break;
+      case "up":
+        data.sort((a, b) => {
+          return a.minCompensation > b.minCompensation ? -1 : 1;
+        });
+        break;
+      case "down":
+        data.sort((a, b) => {
+          return a.minCompensation > b.minCompensation ? 1 : -1;
+        });
+        break;
+    }
+    return data;
+  }
+  sortByPeriodValue(data) {
+    return data.filter((el) => {
+      const dateNow = new Date();
+      dateNow.setDate(dateNow.getDate() - this.searhPeriodValue);
+      if (new Date(dateNow).getTime() < new Date(el.date).getTime()) {
+        return true;
+      }
+    });
+  }
+}
+const FILTR_SORT_component = new FilterAndSort();
+export { FILTR_SORT_component };
---
+++ js/renderVacancies.js
@@ -0,0 +1,85 @@
+import { API_component } from "./api.js";
+import { FILTR_SORT_component } from "./filterAndSort.js";
+import { MODAL_VACANCY } from "./openModalVacancy.js";
+import { SEARCH_component } from "./search.js";
+const resultList = document.querySelector(".wrapper__result");
+const headerLogo = document.querySelector(".middle__logo");
+headerLogo.addEventListener("click", () => {
+  SEARCH_component.removeSearchTitle();
+  VACANCIES_component.removeVacancy();
+  VACANCIES_component.renderVacancy();
+});
+
+class Vacancies {
+  ROOT_element;
+  DATA_VACANCIES = [];
+  constructor(root) {
+    this.ROOT_element = root;
+  }
+  async renderVacancy(dataVacancy) {
+    let data = null;
+    if (dataVacancy) {
+      data = dataVacancy;
+    } else {
+      data = await API_component.getVacancies();
+    }
+    data = FILTR_SORT_component.sortByParams(data);
+    this.DATA_VACANCIES = data;
+    const list = document.createElement("ul");
+    list.className = "result__list";
+
+    const elems = data.map((item) => {
+      const li = document.createElement("li");
+      li.className = "result__item";
+      li.innerHTML = `
+                <article class="vacancy">
+                  <h2 class="vacancy__title">
+                    <a class="vacancy__open-modal" href="#" data-vacancy="${item.id}"
+                      >${item.title}</a
+                    >
+                  </h2>
+                  <p class="vacancy__compensation">
+                    ${item.compensation}
+                  </p>
+                  <p class="vacancy__work-schedule">${item.workSchedule}</p>
+                  <div class="vacancy__employer">
+                    <p class="vacancy__employer-title">${item.employer}</p>
+                    <p class="vacancy__employer-address">${item.address}</p>
+                  </div>
+                  <p class="vacancy__description">
+                  ${item.description}
+                  </p>
+                  <p class="vacancy__date">
+                    <time datetime="2022-02-25">${item.date}</time>
+                  </p>
+                  <div class="vacancy__wrapper-btn">
+                    <a
+                      class="vacancy__response vacancy__open-modal"
+                      data-vacancy="${item.id}"
+                      >Откликнуться</a
+                    >
+                    <button class="vacancy__contacts">Показать контакты</button>
+                  </div>
+                </article>`;
+      return li;
+    });
+    list.append(...elems);
+    this.ROOT_element.append(list);
+
+    this.initListener(list);
+  }
+  removeVacancy() {
+    this.ROOT_element.innerHTML = "";
+  }
+  initListener(HTMLelement) {
+    HTMLelement.addEventListener("click", () => {
+      if (!event.target.dataset.vacancy) {
+        return;
+      }
+      MODAL_VACANCY.renderModal(event.target.dataset.vacancy);
+    });
+  }
+}
+
+const VACANCIES_component = new Vacancies(resultList);
+export { VACANCIES_component };
